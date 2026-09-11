c5c3f2314f40340e11320b4a3b41d0793d16d29d
Devashish <reduma9039@gmail.com>
2026-09-11 13:32:18 +0530

    feat: Phase 1 - project setup with Spring Boot, Postgres, Redis and Maven wrapper

---
+++ src/main/java/com/jobportal/JobPortalApplication.java
@@ -0,0 +1,14 @@
+package com.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class JobPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalApplication.class, args);
+    }
+}
